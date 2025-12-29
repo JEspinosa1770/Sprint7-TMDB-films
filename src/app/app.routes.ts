7b9dcf611c5fefb69690c919c3adcd1c4afa1235
JEspinosa1770 <jespinosa1770@gmail.com>
2025-12-29 18:24:18 +0100

    feat: definition of the general page structure.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { FilmList } from './features/film-list/film-list';
+import { FilmDetail } from './features/film-detail/film-detail';
+import { Login } from './features/login/login';
+import { Register } from './features/register/register';
+import { Component } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: FilmList },
+  { path: 'film', component: FilmDetail },
+  { path: 'login', component: Login },
+  { path: 'register', component: Register},
+  { path: "**", redirectTo: ''}
+];
