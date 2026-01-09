ab2e632d73271bb479a4d57c54f2eee320dfa1b1
JEspinosa1770 <jespinosa1770@gmail.com>
2026-01-09 18:17:01 +0100

    feat: change error image.

--- src/app/core/services/api.ts
+++ src/app/core/services/api.ts
@@ -29,7 +29,7 @@ export class Api {
 
   getImageUrl(path: string | null, size: string = 'w500'): string {
     if (!path) {
-      return 'error.png';
+      return 'error1.png';
     }
     return `https://image.tmdb.org/t/p/${size}${path}`;
   }
